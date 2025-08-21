b713b0444b116d6bc8f18adac84ab734303cb13d
kirkland34 <legacywealthfinancial19@gmail.com>
2025-08-20 22:58:59 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 import "./globals.css";
 import TopNav from "@/components/TopNav";
 import BottomNav from "@/components/BottomNav";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-white text-black">
         <TopNav />
-         <main
-  className="
-    mx-auto max-w-screen-sm px-4
-    pt-[calc(64px+env(safe-area-inset-top))]
-    pb-[calc(64px+env(safe-area-inset-bottom)+64px)]
-  "
->
-
+        <main
+          className="
+            mx-auto max-w-screen-sm px-4
+            pt-[calc(64px+env(safe-area-inset-top))]
+            pb-[calc(64px+env(safe-area-inset-bottom)+64px)]
+          "
+        >
           {children}
         </main>
         <BottomNav />
